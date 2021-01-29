b41e29b3c589fc36e15d62c561bd3d918d1f951a
HarshiniRr <52816212+HarshiniRr@users.noreply.github.com>
2021-01-29 12:40:31 +0530

    changed the name to welocm eto page

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Hello, World from Docker! <br>";
+echo "Hello, Welcome to the page! <br>";
 echo "deployed to AKS! <br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 
